298e0a2f6dde5ea4a93fdc6c000f1046390f9576
mobak88 <mostafa0401@ymail.com>
2022-07-14 12:06:38 +0200

    Removing styled components, adding index css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import GlobalStyle from "./globalStyles";
-import Theme from "./Theme";
-import Header from "./components/header/Header";
-import HeroSection from "./components/hero/HeroSection";
+import Theme from './Theme';
+import Header from './components/header/Header';
+import HeroSection from './components/hero/HeroSection';
 
 function App() {
   return (
     <Theme>
-      <GlobalStyle />
       <Header>Heading</Header>
       <HeroSection />
     </Theme>
