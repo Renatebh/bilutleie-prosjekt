c577a6407d8fe60dfadbd6367635ed2bbedafcae
mobak88 <mostafa0401@ymail.com>
2022-07-14 13:28:15 +0200

    Fixing mobile menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { FaPhoneAlt, FaBars, FaTimes } from 'react-icons/fa';
-import logo from '../../assets/racing (2).png';
-import headerStyles from './Header.module.css';
+import React, { useState } from "react";
+import { FaPhoneAlt, FaBars, FaTimes } from "react-icons/fa";
+import logo from "../../assets/racing (2).png";
+import headerStyles from "./Header.module.css";
 
 const Header = () => {
   const [click, setClick] = useState(false);
@@ -9,16 +9,16 @@ const Header = () => {
 
   return (
     <div className={headerStyles.navbar}>
-      <div className={headerStyles['logo-wrapper']}>
-        <img src={logo} alt='bildet av logoen' />
+      <div className={headerStyles["logo-wrapper"]}>
+        <img src={logo} alt="bildet av logoen" />
         <p className={headerStyles.logo}>Bilutleie</p>
       </div>
 
       <ul
         className={
           click
-            ? headerStyles['nav-menu'] + headerStyles.active
-            : headerStyles['nav-menu']
+            ? headerStyles["nav-menu"] + " " + headerStyles.active
+            : headerStyles["nav-menu"]
         }
       >
         <li>{/* <Link>Personbil</Link> */}</li>
@@ -27,14 +27,14 @@ const Header = () => {
         <li>Kontakt oss</li>
       </ul>
 
-      <div className={headerStyles['nav-icon']} onClick={handleClick}>
+      <div className={headerStyles["nav-icon"]} onClick={handleClick}>
         {click ? <FaTimes size={20} /> : <FaBars size={20} />}
       </div>
 
-      <div className={headerStyles['contact-wrapper']}>
+      <div className={headerStyles["contact-wrapper"]}>
         <FaPhoneAlt size={25} />
 
-        <a href='tel:91774282' className={headerStyles.phone}>
+        <a href="tel:91774282" className={headerStyles.phone}>
           22 00 22 00
         </a>
       </div>
