b906f1265fc7336348df481ee77a96069d6c81d1
Renatebh <renate.hem@gmail.com>
2022-07-16 13:11:11 +0200

    added styling to cards, added new files to footer

--- src/components/cards/CardsData.jsx
+++ src/components/cards/CardsData.jsx
@@ -2,19 +2,19 @@ import { FaMapMarked, FaUsers, FaRocket } from "react-icons/fa";
 
 const InfoCardsData = [
   {
-    imgsrc: <FaRocket size={50} />,
+    imgsrc: <FaRocket size={80} />,
     title: "Kjapp og trygg booking",
-    text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur blanditiis excepturi earum neque non voluptas quisquam iuredebitis eos ut, inventore ad labore obcaecati dolore sequi,consectetur expedita suscipit itaque?",
+    text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur blanditiis excepturi earum neque non voluptas quisquam iuredebitis eos ut, ",
   },
   {
-    imgsrc: <FaMapMarked size={50} />,
+    imgsrc: <FaMapMarked size={80} />,
     title: "Mange hentesteder",
-    text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur blanditiis excepturi earum neque non voluptas quisquam iuredebitis eos ut, inventore ad labore obcaecati dolore sequi,consectetur expedita suscipit itaque?",
+    text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur blanditiis excepturi earum neque non voluptas quisquam iuredebitis eos ut, ",
   },
   {
-    imgsrc: <FaUsers size={50} />,
+    imgsrc: <FaUsers size={80} />,
     title: "Fornøyde kunder og garanti",
-    text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur blanditiis excepturi earum neque non voluptas quisquam iuredebitis eos ut, inventore ad labore obcaecati dolore sequi,consectetur expedita suscipit itaque?",
+    text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur blanditiis excepturi earum neque non voluptas quisquam iuredebitis eos ut, ",
   },
 ];
 
---
+++ src/components/footer/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="contact-container">
+      <p className="company-name">Bilutleie</p>
+      <p>Osloveien 22</p>
+      <p>2200 Oslo</p>
+    </div>
+  );
+};
+
+export default Contact;
