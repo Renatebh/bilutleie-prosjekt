1a292f40f0c87db33f7bda229af5a091e5e62a73
mobak88 <mostafa0401@ymail.com>
2022-09-14 20:01:32 +0200

    Finishing logic for sorting cars based on price

--- src/components/Carousel/carCarousel/FilterCars.jsx
+++ src/components/Carousel/carCarousel/FilterCars.jsx
@@ -3,11 +3,16 @@ import styles from "./FilterCars.module.css";
 import PriceOption from "./filterOptions/PriceOption";
 import BrandOption from "./filterOptions/BrandOption";
 import TypeOption from "./filterOptions/TypeOption";
+import API_CONSTANT_MAP from "../../../api/endpoints";
+import useFetch from "../../../hooks/useFetch";
 
 const FilterCars = () => {
-  const [priceOptionVal, setPriceOptionVal] = useState("price");
-  const [brandOptionVal, setBrandOptionVal] = useState("brand");
-  const [typeOptionVal, setTypeOptionVal] = useState("type");
+  const [priceOptionVal, setPriceOptionVal] = useState("");
+  const [brandOptionVal, setBrandOptionVal] = useState("");
+  const [typeOptionVal, setTypeOptionVal] = useState("");
+  const [cars, setCArs] = useState([]);
+
+  const { loading, err, data } = useFetch(`${API_CONSTANT_MAP.cars}`);
 
   const priceOptionChange = (val) => {
     setPriceOptionVal(val);
@@ -21,9 +26,44 @@ const FilterCars = () => {
     setTypeOptionVal(val);
   };
 
+  const sortCarsByPrice = () => {
+    console.log("sort", priceOptionVal);
+    if (priceOptionVal.toLowerCase() === "stigende") {
+      setCArs((prevState) => {
+        const newState = prevState.sort(
+          (a, b) => parseInt(a.attributes.price) - parseInt(b.attributes.price)
+        );
+        return newState;
+      });
+    }
+
+    if (priceOptionVal.toLowerCase() === "synkende") {
+      setCArs((prevState) => {
+        const newState = prevState.sort(
+          (a, b) => parseInt(b.attributes.price) - parseInt(a.attributes.price)
+        );
+        return newState;
+      });
+    }
+
+    console.log(cars);
+  };
+
   useEffect(() => {
-    console.log(priceOptionVal, brandOptionVal, typeOptionVal);
+    console.log(priceOptionVal.toLowerCase(), brandOptionVal, typeOptionVal);
+    sortCarsByPrice();
   }, [priceOptionVal, brandOptionVal, typeOptionVal]);
+
+  useEffect(() => {
+    if (data) {
+      setCArs([...data.data]);
+      console.log(data.data);
+    }
+  }, [data]);
+
+  /* useEffect(() => {
+    console.log(cars);
+  }, [cars]); */
 
   return (
     <form className={styles.form}>
