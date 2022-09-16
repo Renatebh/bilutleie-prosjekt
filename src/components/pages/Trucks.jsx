f50516f0fc0de03a704fb8df6b657208bc68f0b3
mobak88 <mostafa0401@ymail.com>
2022-09-16 12:26:04 +0200

    Adding filtering to trucks page

--- src/components/pages/Trucks.jsx
+++ src/components/pages/Trucks.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Heading from "../ui/heading/Heading";
 import RadioButtonsCars from "../ui/radioButton/RadioButtonsCars";
 import CarCard from "../cards/cars/CarCard";
@@ -7,18 +7,47 @@ import useFetch from "../../hooks/useFetch";
 import API_CONSTANT_MAP from "../../api/endpoints";
 
 const Trucks = () => {
+  const [cars, setCars] = useState([]);
   const { loading, err, data } = useFetch(`${API_CONSTANT_MAP.cars}`);
+
+  const onRadioBtnClick = (val) => {
+    sortCarsByType(val);
+  };
+
+  const sortCarsByType = (val) => {
+    if (val.toLowerCase() === "alle typer") {
+      const filteredCars = data.data.map((car) => car);
+      setCars(filteredCars);
+      return;
+    }
+
+    const filteredCars = data.data.filter((car) => {
+      return (
+        car.attributes.categories.data[0].attributes.title
+          .trim()
+          .toLowerCase() === val.trim().toLowerCase()
+      );
+    });
+
+    setCars(filteredCars);
+  };
+
+  useEffect(() => {
+    if (data) {
+      setCars([...data.data]);
+    }
+  }, [data]);
 
   return (
     <main className={styles.container}>
       <div className={styles.wrapper}>
         <Heading mainHeading>VAREBIL</Heading>
         <form className={styles["radio-form"]} action="">
-          <RadioButtonsCars />
+          <RadioButtonsCars onRadioBtnClick={onRadioBtnClick} />
         </form>
         <div className={styles["cars-container"]}>
           {data &&
-            data.data.map((car) => {
+            cars.map((car) => {
               return (
                 <CarCard
                   key={car.id}
